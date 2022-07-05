bbdbe63d1129964f45063551ea519eee7b1a40a8
Oleg Iskusnyh <oleg@iskusnyh.pro>
2022-07-05 23:42:05 +0600

    Transactional mail: fix encoded message bug

--- Model/Api/Transaction.php
+++ Model/Api/Transaction.php
@@ -60,10 +60,8 @@ class Transaction
                         'email' => $recipient->getEmail()
                     ],
                     'content' => [
-                        // phpcs:ignore Magento2.Functions.DiscouragedFunction
-                        'plain' => base64_encode($message->getBodyText()),
-                        // phpcs:ignore Magento2.Functions.DiscouragedFunction
-                        'html' => base64_encode($message->getBodyText())
+                        'plain' => $message->getBodyText(),
+                        'html' => $message->getBodyText()
                     ]
                 ];
             }
